15e9ac6dc450e22b4c8365d22aba7c8d30540f34
Tim Crisp <timothycrisp@gmail.com>
2022-05-07 09:00:21 -0500

    add definitions of instance/tiate/tiation and encapsulation

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log('hi')
+
+const tulip = {
+  type: 'flower',
+  petals: 3,
+  stem: true,
+  colors: ['red', 'pink', 'yellow'],
+  bees: true,
+  occasion: ['rainy day', 'Easter', 'peace offering']
+}
+
+const sunflower = {
+  type: 'flower',
+  petals: 24,
+  stem: true,
+  colors: ['golden', 'yellow'],
+  bees: true,
+  occasion: ['health']
+}
+
+// create flower blue print; prototype
+
+// properties / key-value pairs
+// methods / behaviors
+
+// instance: an object that is created by a class
